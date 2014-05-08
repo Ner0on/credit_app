24f436b46a321cc5b8befb5ce68fcd927d187424
Ner0 <vadim05@inbox.ru>
2014-05-09 01:05:39 +0300

    view startUP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CreditApp::Application.config.secret_key_base = '4ecd90bf1da86ad60772c774f10c4cb97bb17d631171cde75b700c46250d0274c2eda24152b29835659829079838e2ba372a866bb4b144f181717d463620f3b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CreditApp::Application.config.session_store :cookie_store, key: '_credit_app_session'
